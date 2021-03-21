6391e907aeb96611178f94f6748b7a507856e091
bkh-son <buikhachason@gmail.com>
2021-03-21 22:56:59 +0700

    change port

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -20,7 +20,7 @@ namespace MyWebApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://localhost:5001/");
                 });
     }
 }
